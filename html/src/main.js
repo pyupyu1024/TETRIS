10879f3490e1a0e8a21c814d8aead6f591f08736
Lano0423 <yk2yd2@daum.net>
2021-11-03 02:52:41 +0900

    second update

---
+++ html/src/main.js
@@ -0,0 +1,69 @@
+class Canvas {
+
+    constructor() {
+        this.canvas = new Array(20);
+        for (let i = 0; i < 20; i++)
+            this.canvas[i] = 0;
+    }
+
+    get(x, y) {
+
+    }
+
+    set(x, y) {
+
+    }
+
+}
+
+class BLOCK {
+
+    constructor() {
+
+    }
+
+    turn() {
+
+    }
+
+    put() {
+
+    }
+
+    del() {
+
+    }
+
+    move() {
+
+    }
+
+    can_put() {
+
+    }
+
+}
+
+class TETRIS {
+
+    constructor() {
+        
+    }
+
+    is_row_clear() {
+
+    }
+
+    clear_line() {
+
+    }
+
+    next_block() {
+        
+    }
+
+    is_game_over() {
+
+    }
+
+}
--- src/io.js
+++ src/io.js
@@ -6,10 +6,10 @@ module.exports = function(app) {
     const server = require('http').createServer(app);
     const socket_io = require('socket.io')(server);
 
-    socket_io.on('connection', socket => {
+    socket_io.on('connection', (socket) => {
 
-        socket.on('enter', code => {
-            
+        socket.on('enter', (code) => {
+
             const room = online.get(code);
 
             if (code && room.length < 2) {
@@ -21,26 +21,35 @@ module.exports = function(app) {
 
                 while (!online.has(code = uuid()));
                 online.set(code, [ socket.id ]);
-
+                
             }
+
+            socket.emit('enter', code);
+            this.code = code;
+        });
+
+        socket.on('move', ([x, y]) => {
+            if (Math.abs(x) > 1 || y > 0) return;
+            const room = online.get(this.code);
+            io.to(room[0]).emit('move', [x, y]);
+            io.to(room[1]).emit('move', [x, y]);
         });
 
         socket.on('disconnect', () => {
 
-            const room = online.get(code);
+            const room = online.get(this.code);
             const i = room.indexOf(socket.id);
 
             if (i !== -1) room.splice(i, 1);
 
-            online.set(code, room);
+            online.set(this.code, room);
+            io.to(room[0]).emit('quit');
 
-            if (room.length === 0)
-                online.delete(code);
+            if (!room[0]) online.delete(this.code);
 
         });
 
     });
 
     return server;
-
-}
+};
